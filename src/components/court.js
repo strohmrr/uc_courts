a901fa595bf26d20c1597aef3e6bbe8d372f6831
Seth White <sethewhite@me.com>
2020-02-22 15:56:57 -0500

    changes

---
+++ src/components/court.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Court = (props) => (
+  <div>
+      <h1>Court {props.name}</h1>
+      
+  </div>
+);
+
+export default Court;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Court from '../components/court'
 
 const HomePage = () => (
   <div>
-    <h1>
-    HomePage
-    </h1>
+    <Court 
+      name="Hello"
+    />
   </div>
 );
 
