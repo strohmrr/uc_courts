4866c64d5f08ee7de19b4b0f5c25364f805bbe85
Seth White <sethewhite@me.com>
2020-02-22 15:31:51 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/LoginPage';
+import Profile from './pages/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <Router>
+        <div className="App">
+            <Switch>
+              <Route path="/" component={HomePage} exact />
+              <Route path="/login" component={Login} exact/>
+              <Route path="/profile" component={Profile} exact/>
+            </Switch> 
+        </div> 
+      </Router>
+      </>
+    );
+  }
 }
 
 export default App;
